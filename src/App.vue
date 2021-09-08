0684c0c4ebbbb49d0d2c0aac2fc20b211a1143e9
FariaSta <davidfarias30@gmail.com>
2021-09-07 23:34:42 -0400

    ready

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img src="./assets/LOGO-SLOGAN-ING-18-1536x702.png" alt="Sefar" style="width: 200px; margin-top: 15px">
+    <h3>Ejemplo PDF en Vue</h3>
+    <WebViewer
+      initialDoc="https://static0planetadelibroscom.cdnstatics.com/libros_contenido_extra/42/41659_Criptomonedas.pdf"
+    />
+  </div>
+</template>
+
+<script>
+import WebViewer from "./components/WebViewer.vue";
+
+export default {
+  name: "App",
+  components: {
+    WebViewer,
+  },
+};
+</script>
+
+<style>
+html,
+body {
+  margin: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 0 auto;
+}
+</style>
